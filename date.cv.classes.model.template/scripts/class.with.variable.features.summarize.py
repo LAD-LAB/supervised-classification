6e72e3fe2381932980735574cef5e694558c0845
Firas Midani <firas.midani@duke.edu>
2015-12-13 22:14:25 -0500

    cleaner output

--- date.cv.classes.model.template/scripts/class.with.variable.features.summarize.py
+++ date.cv.classes.model.template/scripts/class.with.variable.features.summarize.py
@@ -14,6 +14,52 @@
 #  for the computation of a p-value (or Type I error) for the empirical 
 #  model. 
 
+########################################################
+# DESCRIPTION OF SUMMARY ESTIMATES
+#########################################################
+
+# Mean
+# 	Each cross-validation has K-folds. This is the mean of the corresponding K ROC curves. If cross validation is resampled (say M times), this is further averaged across all resamples. 
+#       =\frac{1}{M*K} * \sum_{i=1}^{M*K} AuROC_{i}
+#
+# ModelPooled
+# 	Each cross-validation has K-folds. The probability estimtes (or decision scores) are pooled across the K folds and used to comute a gingle ROC curve for the cross vaidation. 
+# 	If cross validation is resamples( say M times), this is further averaged across all resamples.
+#       =roc_curve(true_labels_{i=1...M*K*(N/K)},probability_Estiamtes_{i=1...*M*K*(n/K)}; where N = # of samples in data
+#
+# Averaged_Estimates_Emp_Metric
+# 	Each cross-validation has K-folds. The probability estimate (or decision score) are pooled across the K folds. If cross validation is resampels, the sample-specific 
+#	probability estimates (or decision scores) are averaged across all resamples. This genreates averaged sample-specific estimates (or scores) which are used to generate an ROC curve. 
+#       for each sample in n=1...N 
+#       	probability_estimate_n = \frac{1}{M} \sum_{i=1}^{M} probability_estimate_{i}
+#       =roc_curve(true_labels_{i=1...N},probability_estimate_n{i=1...N}))
+#  
+# Emp_Metric_LowerBound, Emp_Metric_Median, Emp_Metric_UpperBound
+#       The ModelAveraged-based ROC curve is further bootstrapped and its lower bound, median, and upper bound are recorded. 
+# 
+# MeanNull
+# 	Similar as Mean, but uses the Null Models where the labels in training subsets are shuffled. This value is averaged over all permutations of the Null Model 
+# 	
+# ModelPooledNull
+#	Similar as Pooled_Estimates_Emp_metric, but uses the Null Models wehre the labels in training subsets are shuffled. This value is averaged over all permutations of the Null model.
+#
+# NumIterations
+#	Number of iterations of the empirical model (typically 1)
+# 	
+# NumNullIterations
+#       Number of iterations of the null models (typically at least 50)
+#
+# MeanPvalue
+#       P-value of Mean 
+#
+# ModelPooledPvalue
+#       P-value of ModelPooled
+#
+
+#############################################################################
+# INITIALIZATION AND ARGUMENT HANDLINGN
+#############################################################################
+
 import \
 pandas as pd, \
 numpy  as np, \
@@ -27,60 +73,25 @@ pypath = os.path.dirname(os.path.realpath(sys.argv[0]));
 foo    = imp.load_source('classification_summary_library',pypath+'/class.summary.library.py')
 from classification_summary_library import *
 
-filepath  = sys.argv[1] # parent directory for classifier runs 
-metric    = sys.argv[2] # either of {auroc, acc, mcc} which correspond to area under ROC curve, accuracy, and matthews's correlation coefficient scores
-if len(sys.argv)>3:     # if you select auroc, you also need to define the underlying numbers to compute it. either of the following
+filepath  = sys.argv[1] 	  # parent directory for classifier runs 
+metric    = sys.argv[2] 	  # either of {auroc, acc, mcc} which correspond to area under ROC curve, accuracy, and matthews's correlation coefficient scores
+if len(sys.argv)>3:     	  # if you select auroc, you also need to define the underlying numbers to compute it. either of the following
 	roc_frame = sys.argv[3]   # {probas, scores, mean_probas, mean_scores}
 	bootstrap = sys.argv[4]
 #endif
 
-#########################################################
-# DESCRIPTION OF SUMMARY ESTIMATES
-#########################################################
-
-# Vanilla_Mean_Emp_Metric
-# 	Each cross-validation has K-folds. This is the mean of the corresponding K ROC curves. If cross validation is resampled (say M times), this is further averaged across all resamples. 
-#       =\frac{1}{M*K} * \sum_{i=1}^{M*K} AuROC_{i}
-#
-# Pooled_Estimates_Emp_Metric
-# 	Each cross-validation has K-folds. The probability estimtes (or decision scores) are pooled across the K folds and used to comute a gingle ROC curve for the cross vaidation. 
-# 	If cross validation is resamples( say M times), this is further averaged across all resamples.
-#       =roc_curve(true_labels_{i=1...M*K*(N/K)},probability_Estiamtes_{i=1...*M*K*(n/K)}; where N = # of samples in data
-#
-# Averaged_Estimates_Emp_Metric
-# 	Each cross-validation has K-folds. The probability estimate (or decision score) are pooled across the K folds. If cross validation is resampels, the sample-specific 
-#	probability estimates (or decision scores) are averaged across all resamples. This genreates averaged sample-specific estimates (or scores) which are used to generate an ROC curve. 
-#       for each sample in n=1...N 
-#       	probability_estimate_n = \frac{1}{M} \sum_{i=1}^{M} probability_estimate_{i}
-#       =roc_curve(true_labels_{i=1...N},probability_estimate_n{i=1...N}))
-#  
-# Emp_Metric_Lower_Bound, Emp_Metric_Median, Emp_Metric_Upper_Bound
-#       The Average_Estimates_Emp_Metric-based ROC curve is further bootstrapped and its lower bound, median, and upper bound are recorded. 
-# 
-# Vanilla_Mean_Null_Metric
-# 	Similar as Vanilla_Mean_Emp_Metric, but uses the Null Models where the labels in training subsets are shuffled. This value is averaged over all permutations of the Null Model 
-# 	
-# Pooled_Estimates_Null_Metric
-#	Similar as Pooled_Estimates_Emp_metric, but uses the Null Models wehre the labels in training subsets are shuffled. This value is averaged over all permutations of the Null model.
-#
-# Num_Emp_Iterations
-#	Number of iterations of the empirical model (typically 1)
-# 	
-# Num_Null_Iterations
-#       Number of iterations of the null models (typically at least 50)
-#
-# Vanilla_Mean_Emp_Metric_Pvalue
-#       P-value of Vanilla_Mean_Emp_Metric 
-#
-# Pooled_Estimates_Emp_Metric_Pvalue
-#       P-value of Pooled_Estimates_Emp_Metric
-#
-
-#summary_pnl = pd.DataFrame(columns=['EmpMetricLowerBound','EmpMetricMedian','EmpMetricUpperBound','AvgPooledEmpMetric','AvgEmpMetric','AvgPooledEmpMetric','AvgNullMetric','NumEmpIterations','NumNullIterations','EmpMetricPvalue','PooledEmpMetricPvalue'])
-summary_pnl = pd.DataFrame(columns=['Vanilla_Mean_Emp_Metric','Vanilla_Mean_Null_Metric','Vanilla_Mean_Emp_Metric_Pvalue',\
-				    'Pooled_Estimates_Emp_Metric','Pooled_Estimates_Null_Metric','Pooled_Estimates_Emp_Metric_Pvalue',\
-				    'Average_Estimates_Emp_Metric','Average_Estimates_Emp_Metric_Lower_Bound','Average_Estimates_Emp_Metric_Median','Average_Estimates_Emp_Metric_Upper_Bound', \
-			            'Num_Emp_Iterations','Num_Null_Iterations']);
+summary_pnl = pd.DataFrame(columns=['Mean',\
+				    'MeanNull',\
+				    'MeanPvalue',\
+				    'ModelPooled',\
+			            'ModelPooledNull',\
+		   	            'ModelPooledPvalue',\
+				    'ModelAveraged',\
+				    'ModelAveraged_LowerBound',\
+				    'ModelAveraged_Median',\
+				    'ModelAveraged_UpperBound',\
+			            'NumIterations',\
+			            'NumNullIterations']).astype(float);
 
 if metric=="auroc":
 	if   roc_frame == "probas":
@@ -101,11 +112,11 @@ for nf in range(1,501):
 	if os.path.isfile(empirical):
 		if os.stat(empirical).st_size != 0:
 			nf_summ = pd.read_csv(empirical,sep='\t',header=None,index_col=0);
-			summary_pnl.loc[nf,'Vanilla_Mean_Emp_Metric']     =      np.mean(nf_summ.iloc[0,idx_p2]);
-			summary_pnl.loc[nf,'Pooled_Estimates_Emp_Metric'] =      np.mean(nf_summ.iloc[0,idx_p1]);
-			summary_pnl.loc[nf,'Num_Emp_Iterations']          = len(np.where(nf_summ.iloc[:,idx_p1])[0]); 
-			Vanilla_Mean_Emp_Metric     = nf_summ.iloc[0,idx_p2];
-			Pooled_Estimates_Emp_Metric = nf_summ.iloc[0,idx_p1];
+			summary_pnl.loc[nf,'Mean']           =              np.mean(nf_summ.iloc[0,idx_p2]);
+			summary_pnl.loc[nf,'ModelPooled']    =              np.mean(nf_summ.iloc[0,idx_p1]);
+			summary_pnl.loc[nf,'NumIterations']  = str(int(len(np.where(nf_summ.iloc[:,idx_p1])[0]))); 
+			Mean        = nf_summ.iloc[0,idx_p2];
+			ModelPooled = nf_summ.iloc[0,idx_p1];
 		#endif
 	#endif
 
@@ -139,15 +150,15 @@ for nf in range(1,501):
 			list_labels                   = ['trues','mean_scores','mean_probas','mean_predictions'];
 			y_true,y_fit_s,y_fit_p,y_pred = [pd.Series(x.apply(np.mean,1),name=y) for x,y in zip(list_dict,list_labels)];
 			
-			mean_predictions_df = pd.DataFrame([y_true,y_fit_s,y_fit_p,y_pred]);
-			mean_predictions_df.to_csv(filepath+'/model_predictions_df.txt',sep='\t',header=True,index_col=True);
+			pd.DataFrame([y_true,y_fit_s,y_fit_p,y_pred]).to_csv(pickle_path+'model_predictions_df.txt',sep='\t',header=True,index_col=True);
 
 			# bootstrap the averaged decision scores to bootstrap the ROC curve and its corresponding area	
 			y_score = y_fit_s.values;
 			y_pred  = y_pred.values;
 			y_true  = y_true.values;
 	
-			y_true_df, y_pred_df, y_fit_df       = bootstrapClassifierFit(y_true,y_pred,y_score,n_bootstraps,True)	 
+			y_true_df, y_pred_df, y_fit_df = bootstrapClassifierFit(y_true,y_pred,y_score,n_bootstraps,True)	 
+			
 			for i in range(n_bootstraps):
 				BootstrappedClassifierPerformance = ClassifierPerformance(y_true_df.iloc[:,i], y_pred_df.iloc[:,i], y_fit_df.iloc[:,i]);
 				BS_auc_.append(BootstrappedClassifierPerformance.Metrics().auc)
@@ -155,9 +166,9 @@ for nf in range(1,501):
 			confidence_lower, confidence_median, confidence_upper = bootstrappedConfidenceIntervals(BS_auc_,0.05);
 
 			# record bootstraped confidence lower, median, and upper bounds
-			summary_pnl.loc[nf,'Average_Estimates_Emp_Metric_Lower_Bound'] = confidence_lower;
-			summary_pnl.loc[nf,'Average_Estimates_Emp_Metric_Upper_Bound'] = confidence_upper; 
-			summary_pnl.loc[nf,'Average_Estimates_Emp_Metric_Median']     = confidence_median;
+			summary_pnl.loc[nf,'ModelAveraged_LowerBound'] = confidence_lower;
+			summary_pnl.loc[nf,'ModelAveraged_UpperBound'] = confidence_upper; 
+			summary_pnl.loc[nf,'ModelAveraged_Median']     = confidence_median;
 	
 	##########################################################
 	# COMPUTE SUMMARY ESTIMATES OF NULL MODELS
@@ -166,17 +177,15 @@ for nf in range(1,501):
 	if os.path.isfile(permutation):
 		if os.stat(permutation).st_size != 0:
 			nf_summ = pd.read_csv(permutation,sep='\t',header=None,index_col=0);
-			summary_pnl.loc[nf,'Vanilla_Mean_Null_Metric']            =      np.mean(nf_summ.iloc[:,idx_p2]);
-			summary_pnl.loc[nf,'Pooled_Estimates_Null_Metric']         =      np.mean(nf_summ.iloc[:,idx_p1]);
-			summary_pnl.loc[nf,'Num_Null_Iterations'] = len(np.where(nf_summ.iloc[:,idx_p1])[0]);
-			summary_pnl.loc[nf,'Vanilla_Mean_Emp_Metric_Pvalue']      = float(len(np.where(nf_summ.iloc[:,idx_p2]>Vanilla_Mean_Emp_Metric)[0]));
-			summary_pnl.loc[nf,'Vanilla_Mean_Emp_Metric_Pvalue']     /=      (len(np.where(nf_summ.iloc[:,idx_p2])[0])+1);
-			summary_pnl.loc[nf,'Pooled_Estimates_Emp_Metric_Pvalue']  = float(len(np.where(nf_summ.iloc[:,idx_p1]>Pooled_Estimates_Emp_Metric)[0]));
-			summary_pnl.loc[nf,'Pooled_Estimates_Emp_Metric_Pvalue'] /=      (len(np.where(nf_summ.iloc[:,idx_p1])[0])+1);
-#			summary_pnl.loc[nf,'Pooled_Estimates_Emp_Metric_Pvalue']  = ("%0.4f" summary_pnl.loc[nf,'Pooled_Estm
+			summary_pnl.loc[nf,'MeanNull']           =              np.mean(nf_summ.iloc[:,idx_p2]);
+			summary_pnl.loc[nf,'ModelPooledNull']    =              np.mean(nf_summ.iloc[:,idx_p1]);
+			summary_pnl.loc[nf,'NumNullIterations']  = str(int(len(np.where(nf_summ.iloc[:,idx_p1])[0])));
+			summary_pnl.loc[nf,'MeanPvalue']         =   float(len(np.where(nf_summ.iloc[:,idx_p2]>Mean)[0]));
+			summary_pnl.loc[nf,'MeanPvalue']        /=        (len(np.where(nf_summ.iloc[:,idx_p2])[0])+1);
+			summary_pnl.loc[nf,'ModelPooledPvalue']  =   float(len(np.where(nf_summ.iloc[:,idx_p1]>ModelPooled)[0]));
+			summary_pnl.loc[nf,'ModelPooledPvalue'] /=        (len(np.where(nf_summ.iloc[:,idx_p1])[0])+1);
 		#endif
 	#endif
 
-summary_pnl.iloc[:,:-2].astype(float)
 summary_pnl.to_csv(filepath+'/summary/class.two.stage.rfe.summary.'+metric+'.txt',sep='\t',header=True,index_col=True,float_format='%0.4f')
 
